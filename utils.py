a91995adb21c4d5aa65af264f22f3771475a460f
YHS <iyhs1858@gmail.com>
2024-01-19 20:13:25 +0900

    utils: time log

---
+++ utils.py
@@ -0,0 +1,16 @@
+import time
+
+CUR_TIME = time.time()
+
+def time_log(job, start_time=0):
+    
+    global CUR_TIME
+    
+    if start_time != 0:
+        print('%s time: %.6f sec' % (job, time.time() - start_time))    
+    else:
+        print('%s time: %.6f sec' % (job, time.time() - CUR_TIME))
+    
+    CUR_TIME = time.time()
+
+    print()
